5a03077cc137382cb91d143f10f82b02f60c52e3
nikolai0802 <46624533+nikolai0802@users.noreply.github.com>
2019-04-23 20:46:12 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
 import React from 'react';
-import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
+import {Div, Group, Panel, PanelHeader, View} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+class App extends React.Component {
 
-class Example extends React.Component {
-
-  constructor () {
-    this.state = {
-      activePanel: 'brand'
+    render() {
+        return (
+            <View activePanel="mainPanel">
+                <Panel id="mainPanel">
+                    <PanelHeader>Example</PanelHeader>
+                    <Group>
+                        <Div>
+                            Hello world
+                        </Div>
+                    </Group>
+                </Panel>
+            </View>
+        );
     }
-  }
-
-  render () {
-
-    return (
-      <View activePanel={this.state.activePanel}>
-        <Panel id="brand">
-          <PanelHeader>
-            Настройки
-          </PanelHeader>
-          <Group>
-            <CellButton onClick={() => this.setState({ activePanel: 'light' })}>Альтернативная тема</CellButton>
-          </Group>
-        </Panel>
-        <Panel id="light">
-          <PanelHeader
-            theme="light"
-            left={<HeaderButton onClick={() => this.setState({ activePanel: 'brand' })}>{osname === IOS ? <Icon28ChevronBack /> : <Icon24Back />}</HeaderButton>}
-            addon={<HeaderButton onClick={() => this.setState({ activePanel: 'brand' })}>Назад</HeaderButton>}
-          >
-            Котики
-          </PanelHeader>
-        </Panel>
-      </View>
-    )
-  }
 }
 
-<Example />
+export default App;
