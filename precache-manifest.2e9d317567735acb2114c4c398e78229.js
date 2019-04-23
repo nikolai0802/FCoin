71481c19b61fe7c7afbaa5afabc07be9aa9d3072
Nikolai <makarov.nikolai01@gmail.com>
2019-04-23 16:56:10 +0500

    Updates

---
+++ precache-manifest.2e9d317567735acb2114c4c398e78229.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "46e9e1de031f3744d819",
+    "url": "/FCoin/static/css/main.6c27e415.chunk.css"
+  },
+  {
+    "revision": "46e9e1de031f3744d819",
+    "url": "/FCoin/static/js/main.46e9e1de.chunk.js"
+  },
+  {
+    "revision": "f0456c81fd81bf006516",
+    "url": "/FCoin/static/js/runtime~main.f0456c81.js"
+  },
+  {
+    "revision": "901e524b15d814b99a34",
+    "url": "/FCoin/static/css/2.890fc26c.chunk.css"
+  },
+  {
+    "revision": "901e524b15d814b99a34",
+    "url": "/FCoin/static/js/2.901e524b.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/FCoin/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "ab534d463d184992a3a83044eb276d05",
+    "url": "/FCoin/index.html"
+  }
+];
